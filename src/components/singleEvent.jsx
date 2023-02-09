d461ba109af93678318ba190d0ee968c4750f3b4
Hariiikrishnan <therihari36@gmail.com>
2023-02-10 00:32:01 +0530

    somewhat head cont

---
+++ src/components/singleEvent.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function singleEvent(){
+    return <div>
+       <h2 class="center">Web Hacking</h2>
+        <h4 class="center">Rules to Remember!</h4>
+        <div class="ruleslist center"> 
+        <ul>
+          <li>Abbra Kaah daabraa joo mandhra kaali haa manavum vikkon gaaliii.</li>
+          <li>thillana thillana naan thithikindra thena.thiku thiku nenju thillana..</li>
+          <li>nenjikul peidhidum maa malai neerukkul moolgidum thamaarai..</li>
+      </ul>
+      </div>
+    </div>
+}
+
+export default singleEvent;
